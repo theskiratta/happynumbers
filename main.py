398fd1ed8e1ad1533d15aeb574d9e412354b0f61
theskirata <patrick.riebe1234@gmail.com>
2022-03-26 11:28:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#####################
+### Happy Numbers ###
+#####################
+
+def sumSquareOfDigits(n):
+    #print("Summing square of digits... ")
+    sum = 0
+    while n != 0:
+        sum = sum + (n % 10)**2
+        n = n // 10
+
+    return sum
+
+def isHappy(n):
+    happy = True
+    values = []
+    while n != 1: # As long as we don't reach 1 as value or a result is repeated, we keep going
+        value = sumSquareOfDigits(n)
+        if value in values: # Repeated result, number can't possibly be happy
+            happy = False
+            break
+        else: # This value is not in the list yet, so we add it to the list
+            values.append(value)
+            n = value
+
+    return happy
+
+number = int(input("Please enter an integer: "))
+# Now send number to isHappy() and print result
+print(isHappy(number))
